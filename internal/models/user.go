f378196877bb459f1d40c4bbf17d880ac1be543c
asd1asd00000 <126613068+asd1asd00000@users.noreply.github.com>
2026-07-06 02:02:39 +0330

    Update user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,19 +1,27 @@
 package models
 
-// ساختار دقیق کاربر
-type User struct {
-	Username   string   `json:"username"`
-	URLs       []string `json:"urls"`
-	CreatedAt  int64    `json:"created_at"`
-	LastActive int64    `json:"last_active"`
+// ساختار نودها (سرورهای اصلی GuardCore)
+type Node struct {
+	URL      string `json:"url"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
-// ساختار تنظیمات کل سیستم
+// ساختار دقیق کاربر با اضافه شدن محدودیت حجم
+type User struct {
+	Username    string   `json:"username"`
+	URLs        []string `json:"urls"`
+	CreatedAt   int64    `json:"created_at"`
+	LastActive  int64    `json:"last_active"`
+	VolumeLimit int64    `json:"volume_limit"` // حجم مجاز به بایت
+}
+
+// ساختار تنظیمات کل سیستم با اضافه شدن لیست نودها
 type SystemSettings struct {
 	AdminUsername    string `json:"admin_username"`
 	AdminPassword    string `json:"admin_password"`
 	
-	BackupInterval   int    `json:"backup_interval"` // زمان بکاپ خودکار (به ساعت)
+	BackupInterval   int    `json:"backup_interval"`
 	
 	TelegramToken    string `json:"token"`
 	TelegramChatID   string `json:"chat_id"`
@@ -22,8 +30,10 @@ type SystemSettings struct {
 	SmtpEmail        string `json:"smtp_email"`
 	SmtpPassword     string `json:"smtp_password"`
 	SmtpReceiver     string `json:"smtp_receiver"`
-	SmtpZipPassword  string `json:"smtp_zip_password"` // رمز اختصاصی فایل فشرده ایمیل
+	SmtpZipPassword  string `json:"smtp_zip_password"`
 	
 	TutorialsURL     string `json:"tutorials_url"`
 	AnnouncementsURL string `json:"announcements_url"`
+	
+	Nodes            []Node `json:"nodes"` // لیست نودهای متصل به سیستم
 }
